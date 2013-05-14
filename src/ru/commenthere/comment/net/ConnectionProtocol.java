077d4af473c5eca4812cf4d87d083ede1b69c130
Andrey Pelipec <pelipec@mail.ru>
2013-05-14 14:34:50 +0300

    Added verifyCode method to API

--- src/ru/commenthere/comment/net/ConnectionProtocol.java
+++ src/ru/commenthere/comment/net/ConnectionProtocol.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import ru.commenthere.comment.AppContext;
+import ru.commenthere.comment.model.User;
 
 
 import android.net.Uri;
@@ -18,6 +19,7 @@ public class ConnectionProtocol {
 	
 	
 	private final static String SEND_CODE_ACTION = "send_code";
+	private final static String VERIFY_CODE_ACTION = "verify_code";
 	
 	private final static int OK_CODE = 0;
 	
@@ -25,6 +27,9 @@ public class ConnectionProtocol {
 	private final static String RESULT_PARAM_NAME = "result";
 	private final static String ACTION_PARAM_NAME = "act";
 	private final static String EMAIL_PARAM_NAME = "email";
+	private final static String CODE_PARAM_NAME = "code";	
+	private final static String TOKEN_PARAM_NAME = "token";
+	private final static String ID_PARAM_NAME = "id";
 
 	private ConnectionClient client;
 	private String baseUrl;
@@ -64,6 +69,40 @@ public class ConnectionProtocol {
 		} catch (JSONException e) {
 			throw new ConnectionClientException("JSONException", e);
 		}
+	}
+	
+	private User verifyCode(String email, String code) throws ConnectionClientException{
+		User user = null;
+		
+		Uri.Builder ub = Uri.parse(AppContext.API_URL).buildUpon();
+		ub.appendQueryParameter(ACTION_PARAM_NAME, VERIFY_CODE_ACTION);
+		ub.appendQueryParameter(EMAIL_PARAM_NAME, email);
+		ub.appendQueryParameter(CODE_PARAM_NAME, code);
+		
+		String response = client.sendGetRequest(ub.toString());
+		
+		JSONObject result = null; 
+		
+		try {
+			result = new JSONObject(response);
+			int errorCode = result.optInt(ERROR_PARAM_NAME);
+			if( errorCode == OK_CODE){
+				JSONObject actionResult = result.getJSONObject(VERIFY_CODE_ACTION); 
+				String token = actionResult.optString(TOKEN_PARAM_NAME);
+				Integer userId = actionResult.optInt(ID_PARAM_NAME);
+				user = new User();
+				user.setEmail(email);
+				user.setId(userId);
+				user.setToken(token);
+			} else{
+				throw new ConnectionClientException("Server error code: "+ errorCode);
+			}
+			
+		} catch (JSONException e) {
+			throw new ConnectionClientException("JSONException", e);
+		}
+		
+		return user;
 	}
 	
 
